2b66c60fab0c8e831dc53c8415d2bda8a8c095dd
superfilar <mateusz9650@gmail.com>
2024-01-31 21:24:31 +0100

    hw03 v1.0

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import App from './App';
+import '../src/styles.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
